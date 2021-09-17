4e54ce7fee32e9c13bce376bfacf6ff1486f097d
Andrew Taylor <awtcn4@umsystem.edu>
2021-09-17 11:55:21 -0500

    First Commit

---
+++ license.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getlicense(void){
+
+}
+
+int returnlicense(void){
+
+}
+
+int initlicense(void){
+
+}
+
+addtolicense(int n){
+
+}
+
+removelicense(int n){
+
+}
+
+logmsg(const char * msg){
+
+}
