a2faf6f955c7b95593904a9319d7888876168c47
Andrew Taylor <awtcn4@umsystem.edu>
2021-09-25 11:01:22 -0500

    Update

--- license.c
+++ license.c
@@ -1,26 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+class License
+{
+	private:
+		int nlicenses;
+	public:
 
-int getlicense(void){
+		int getlicense(void){
+			while(nlicenses == 0);
+			return(0);
+		}		
 
-}
+		int returnlicense(void){
+			nlicenses++;
+			return(0);
+		}	
 
-int returnlicense(void){
+		int initlicense(void){
+			//do something
+		}
 
-}
+		addtolicense(int n){
+			nlicenses += n;
+		}
 
-int initlicense(void){
+		removelicense(int n){
+			nlicenses -= n;
+		}
 
-}
+		logmsg(const char * msg){
 
-addtolicense(int n){
-
-}
-
-removelicense(int n){
-
-}
-
-logmsg(const char * msg){
-
+		}
 }
---
+++ license.h
@@ -0,0 +1,12 @@
+#ifndef LICENSE_H
+#define LICENSE_H
+
+class license {
+	int getlicense(void);
+	int returnlicense(void);
+	int initlicense(void);
+	addtolicense(int n);
+	removelicense(int n);
+	logmsg(const char* msg);
+}
+#endif
